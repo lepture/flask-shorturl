722ccc9ca85b0bbb8225bb034702ab2990f033b0
Hsiaoming Yang <me@lepture.com>
2013-04-23 20:04:03 +0800

    publish 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='Flask-ShortUrl',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/lepture/flask-shorturl',
     author='Hsiaoming Yang',
     author_email='me@lepture.com',
