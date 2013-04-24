4798c83d801729d2582a16db9c932ad0fcf22bdc
Hsiaoming Yang <me@lepture.com>
2013-04-25 00:24:13 +0800

    more test case. full coverage

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from nose.tools import raises
 from flask import Flask
 from flask_shorturl import ShortUrl
 
@@ -15,3 +16,46 @@ def test_shorturl():
         assert b == d
         assert c == su.encode_url(a)
         assert a == su.decode_url(c)
+
+
+def test_init_app():
+    app = Flask(__name__)
+    su = ShortUrl()
+    su.init_app(app)
+
+    a = 21
+    b = su.encode(a)
+    c = su.enbase(b)
+    d = su.debase(c)
+    e = su.decode(d)
+    assert a == e
+    assert b == d
+    assert c == su.encode_url(a)
+    assert a == su.decode_url(c)
+
+
+def test_flask_ctx():
+    app = Flask(__name__)
+    DEFAULT_ALPHABET = 'mn6j2c4rv8bpygw95z7hsdaetxuk3fq'
+    app.config.setdefault('SHORT_URL_ALPHABET', DEFAULT_ALPHABET)
+    app.config.setdefault('SHORT_URL_MIN_LENGTH', 5)
+    app.config.setdefault('SHORT_URL_BLOCK_SIZE', 24)
+
+    su = ShortUrl()
+
+    with app.test_request_context():
+        a = 21
+        b = su.encode(a)
+        c = su.enbase(b)
+        d = su.debase(c)
+        e = su.decode(d)
+        assert a == e
+        assert b == d
+        assert c == su.encode_url(a)
+        assert a == su.decode_url(c)
+
+
+@raises(RuntimeError)
+def test_no_ctx():
+    su = ShortUrl()
+    su.encode(21)
